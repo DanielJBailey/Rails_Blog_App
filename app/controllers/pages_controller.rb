e0bf694372cb81c9f9f4ff6a6e5d7a8bca28cf61
Daniel Bailey <danielbailey@Daniels-MacBook-Pro.local>
2018-11-12 14:05:52 -0700

    Add root support and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,31 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
     # render index behind the scenes
+    # render :index
   end
 
   def show
     @page = Page.find(params[:id])
+    # render :show
   end
 
   def new
     @page = Page.new
+    # render :new
   end
 
   def create
+    @page = Page.new(pages_params)
 
+    if (@page.save)
+      redirect_to pages_path
+    else
+      render :new
+    end
   end
+
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
